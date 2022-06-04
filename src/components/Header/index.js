64fd320e5a839a96a3bb42e6d21c21fca9250768
JessMotta <jessicalimamotta@gmail.com>
2022-06-04 20:41:38 -0300

    fix width in menu

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -4,7 +4,7 @@ import Menu from '../Menu'
 export default function Header() {
   return (
     <header className='bg-english-violet text-white flex items-center justify-around h-24'>
-        <div>logo</div>
+        <div>LOGO</div>
         <Menu/>
     </header>
   )
