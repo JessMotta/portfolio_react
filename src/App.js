0f9eae755355235a73a987b691bfbcca45649592
JessMotta <jessicalimamotta@gmail.com>
2022-06-07 18:48:17 -0300

    start portfolio section

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import "./global.css"
 import Header from "./components/Header";
 import Hero from "./components/Hero"
 import About from "./components/About"
-// import Portfolio from "./components/Portfolio";
+import Portfolio from "./components/Portfolio";
 import Github from "./components/Github";
 import Footer from "./components/Footer";
 
@@ -14,7 +14,7 @@ function App() {
       <Header />
       <Hero/>
       <About/>
-      {/* <Portfolio/> */}
+      <Portfolio/>
       <Github/>
       <Footer/>
     </>
--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,9 +1,104 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+
 
 export default function Portfolio() {
+  const [project, setProject] = useState([]);
+
+  useEffect(() => {
+    fetch("./database.json", {
+      headers: {
+        Accept: "application/json",
+      },
+    })
+      .then((res) => res.json())
+      .then((data) => projects(data));
+  }, []);
+
+  function projects(data) {
+    setProject(data);
+    console.log(data);
+  }
+
   return (
-    <section>
-        <h2 className="my-40 text-english-violet text-4xl font-bold text-center">Portfólio</h2>
+    <section className="mx-8">
+      <h2 className="my-40 text-english-violet text-4xl font-bold text-center">
+        Portfólio
+      </h2>
+      <div className="flex justify-between">
+      <div className="w-80 bg-english-violet py-5 px-3 rounded-lg">
+        <a
+          href="#"
+          aria-label="link para o site da Agatha Christie"
+        >
+          <div>
+            <img
+              className="w-80"
+              src="https://i.postimg.cc/Hx8HLBNj/agatha-christie.png"
+              alt="imagem do site da Agatha Christie"
+            />
+            <h2 className="text-misty-rose text-center text-xl font-bold mt-3">
+              Desafio do Dia das Mulheres da FCamara
+            </h2>
+          </div>
+        </a>
+      </div>
+
+      <div className="w-80 bg-english-violet py-5 px-3 rounded-lg">
+        <a
+          href="https://jessmotta.github.io/agathaChristie_challengeFCamara/"
+          aria-label="link para o site da Agatha Christie"
+        >
+          <div>
+            <img
+              className="w-80"
+              src="https://i.postimg.cc/Hx8HLBNj/agatha-christie.png"
+              alt="imagem do site da Agatha Christie"
+            />
+            <h2 className="text-misty-rose text-center text-xl font-bold mt-3">
+              Desafio do Dia das Mulheres da FCamara
+            </h2>
+          </div>
+        </a>
+      </div>
+
+      <div className="w-80 bg-english-violet py-5 px-3 rounded-lg">
+        <a
+          href="https://jessmotta.github.io/agathaChristie_challengeFCamara/"
+          aria-label="link para o site da Agatha Christie"
+        >
+          <div>
+            <img
+              className="w-80"
+              src="https://i.postimg.cc/Hx8HLBNj/agatha-christie.png"
+              alt="imagem do site da Agatha Christie"
+            />
+            <h2 className="text-misty-rose text-center text-xl font-bold mt-3">
+              Desafio do Dia das Mulheres da FCamara
+            </h2>
+          </div>
+        </a>
+      </div>
+
+      <div className="w-80 bg-english-violet py-5 px-3 rounded-lg">
+        <a
+          href="https://jessmotta.github.io/agathaChristie_challengeFCamara/"
+          aria-label="link para o site da Agatha Christie"
+        >
+          <div>
+            <img
+              className="w-80"
+              src="https://i.postimg.cc/Hx8HLBNj/agatha-christie.png"
+              alt="imagem do site da Agatha Christie"
+            />
+            <h2 className="text-misty-rose text-center text-xl font-bold mt-3">
+              Desafio do Dia das Mulheres da FCamara
+            </h2>
+          </div>
+        </a>
+      </div>
+
+      </div>
+    
     </section>
-  )
+  );
 }
