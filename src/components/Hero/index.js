f1841a3e3f32ae083cd16cff03d0738be4d9f78d
JessMotta <jessicalimamotta@gmail.com>
2022-06-07 10:05:48 -0300

    finish hero section

--- src/components/Hero/index.js
+++ src/components/Hero/index.js
@@ -2,9 +2,9 @@ import React from "react";
 
 export default function index() {
   return (
-    <section className="bg-hero-pattern bg-cover bg-neutral-700 h-screen">
-      <h1 className="text-misty-rose flex justify-center pt-20 text-5xl">Desenvolvedora Front-end</h1>
-      <h2 className="text-misty-rose flex justify-center pt-20 text-3xl">Transformo seus projetos em aplicações web ou mobile!</h2>
+    <section className="bg-hero-pattern bg-blend-luminosity bg-cover h-screen text-white text-center py-32 m-auto">
+      <h1 className="flex justify-center text-6xl font-bold w-2/3 m-auto">Oi, meu nome é Jéssica, sou Desenvolvedora Front-end</h1>
+      <h2 className="flex justify-center pt-20 text-4xl w-1/2 m-auto">E transformo seus projetos em aplicações reais, web ou mobile!</h2>
     </section>
   );
 }
--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -4,10 +4,10 @@ export default function Menu() {
   return (
     
         <ul className='flex items-center text-xl'>
-            <li className='mx-4'><a className='cursor-pointer hover:text-zinc-400 duration-200'>Home</a></li>
-            <li className='mx-4'><a className='cursor-pointer hover:text-zinc-400 duration-200'>Sobre</a></li>
-            <li className='mx-4'><a className='cursor-pointer hover:text-zinc-400 duration-200'>Portfólio</a></li>
-            <li className='mx-4'><a className='cursor-pointer hover:text-zinc-400 duration-200'>Contato</a></li>
+            <li className='mx-4'><a className='cursor-pointer hover:text-zinc-400 duration-200' href='https://jessicamottadev.com'>Home</a></li>
+            <li className='mx-4'><a className='cursor-pointer hover:text-zinc-400 duration-200' href='https://jessicamottadev.com'>Sobre</a></li>
+            <li className='mx-4'><a className='cursor-pointer hover:text-zinc-400 duration-200' href='https://jessicamottadev.com'>Portfólio</a></li>
+            <li className='mx-4'><a className='cursor-pointer hover:text-zinc-400 duration-200' href='https://jessicamottadev.com'>Contato</a></li>
         </ul>
 
   )
